ee73fb5214beba171dc7378221c573ae829a3ed9
RobbiAbd <robbyabd10@gmail.com>
2020-03-26 17:54:29 +0700

    info covid 19

---
+++ js/covid-19.js
@@ -0,0 +1,113 @@
+getDataIndonesia()
+.then(data => updateUiMenu(data));
+
+
+getDataProvinsi()
+.then(data => updateUiTableProvinsi(data));
+
+
+async function getDataIndonesia() {
+	return await fetch('https://api.kawalcorona.com/indonesia/')
+	.then(resp => resp.json())
+	.then(resp => resp);
+}
+
+async function getDataProvinsi() {
+	return await fetch('https://cors-anywhere.herokuapp.com/https://api.kawalcorona.com/indonesia/provinsi')
+	.then(resp => resp.json())
+	.then(resp => resp);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function updateUiMenu(data) {
+	let html = '';
+	data.forEach(dt => {
+		html = `<div class="col-md-4 mt-3">
+		<div class="card bg-danger">
+		<div class="card-body">
+		<div class="d-flex">
+		<div class="text-white">
+		<p class="text-white mb-0">TOTAL POSITIF</p>
+		<h2 class="mb-0 number-font">${dt.positif}</h2>
+		<p class="text-white mb-0">ORANG</p>
+		</div>
+		<div class="ml-auto"> <i class="far fa-frown fa-5x"></i> </div>
+		</div>
+		</div>
+		</div>
+		</div>
+
+		<div class="col-md-4 mt-3">
+		<div class="card bg-success">
+		<div class="card-body">
+		<div class="d-flex">
+		<div class="text-white">
+		<p class="text-white mb-0">TOTAL SEMBUH</p>
+		<h2 class="mb-0 number-font">${dt.sembuh}</h2>
+		<p class="text-white mb-0">ORANG</p>
+		</div>
+		<div class="ml-auto"> <i class="far fa-smile fa-5x"></i> </div>
+		</div>
+		</div>
+		</div>
+		</div>
+
+		<div class="col-md-4 mt-3 mb-5">
+		<div class="card bg-warning">
+		<div class="card-body">
+		<div class="d-flex">
+		<div class="text-white">
+		<p class="text-white mb-0">TOTAL MENINGGAL</p>
+		<h2 class="mb-0 number-font">${dt.meninggal}</h2>
+		<p class="text-white mb-0">ORANG</p>
+		</div>
+		<div class="ml-auto"> <i class="far fa-sad-tear fa-5x"></i> </div>
+		</div>
+		</div>
+		</div>
+		</div>`;
+	});
+
+	const menu = document.querySelector('.menu-data');
+	menu.innerHTML = html;
+}
+
+
+function updateUiTableProvinsi(data) {
+	let html = '';
+	data.forEach((dt,i) => {
+		html += `<tr>
+      <th scope="row">${i + 1}</th>
+      <td>${dt.attributes.Provinsi}</td>
+      <td>${dt.attributes.Kasus_Posi}</td>
+      <td>${dt.attributes.Kasus_Semb}</td>
+      <td>${dt.attributes.Kasus_Meni}</td>
+    </tr>`;
+	});
+	const table = document.querySelector('.table-data-provinsi');
+	table.innerHTML = html;
+}
